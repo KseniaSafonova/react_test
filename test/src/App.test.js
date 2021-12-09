785c157c5f2d9c6ea1ae268902e8048533043e31
KseniaSafonova <kseniatsn@gmail.com>
2021-12-09 18:00:22 +0300

    +

--- test/src/App.test.js
+++ test/src/App.test.js
@@ -2,15 +2,24 @@ import { render, screen } from '@testing-library/react';
 import App, { sum } from './App';
 
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/for/i);
-  expect(linkElement).toBeInTheDocument();
-  // const { asFragment } = render(<App />);
-  // expect(asFragment(<App />)).toMatchSnapshot();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   // const linkElement = screen.getByText(/for/i);
+//   // expect(linkElement).toBeInTheDocument();
+//   const { asFragment } = render(<App />);
+//   expect(asFragment(<App />)).toMatchSnapshot();
+// });
 
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+
+describe('App', () => {
+  it('renders App component', () => {
+    render(<App />);
+    // screen.debug();
+    screen.getByText('Search:');
+  })
+})
