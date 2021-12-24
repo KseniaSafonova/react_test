8782f2fb10a679ac7e148f4e9056d20327c5f6e4
KseniaSafonova <kseniatsn@gmail.com>
2021-12-24 16:45:20 +0300

    payload

---
+++ test/src/frontend.test.js
@@ -0,0 +1,28 @@
+// **Payload.test.js**
+// import { render, screen, fireEvent } from '@testing-library/react';
+// import Payload from './Payload';
+
+
+// describe('Payload', () => {
+//     it('renders Payload component', () => {
+//         render(<Payload />);
+//         screen.debug();
+//         fireEvent.change(screen.getByRole('textbox'), {
+//             target: { value: 'React' }
+//         });
+//         screen.debug();
+//     })
+// })
+
+// describe('Click', () => {
+//     it('renders Payload button component', () => {
+//         render(<Payload />);
+//         screen.debug();
+//         const button = screen.getByText('Показать');
+//         fireEvent.click(button);
+//         screen.debug();
+//     })
+// })
+
+
+
